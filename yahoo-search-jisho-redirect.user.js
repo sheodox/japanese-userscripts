54108de00977e2fda33f380eb09c4a4232c64ba5
sheodox <imagamer27@msn.com>
2017-05-28 00:08:17 -0500

    add yahoo search jisho redirect userscript

---
+++ yahoo-search-jisho-redirect.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Yahoo Search Jisho Redirect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Redirects yahoo searches to jisho searches for easy lookups from MCBookViewer
+// @author       You
+// @match        https://dic.yahoo.co.jp/search/?ei=UTF-8&p=*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const terms = (location.search.match(/p=(.*)/)[1]).replace('&stype=prefix&fr=dic', '');
+    location.replace('http://jisho.org/search/' + terms.trim());
+})();
