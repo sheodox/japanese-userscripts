dfccf7a305c8d6f857bd2bb2b5310f1f14258a70
sheodox <imagamer27@msn.com>
2019-01-04 20:13:23 -0600

    trimming superfluous characters

--- yahoo-search-jisho-redirect.user.js
+++ yahoo-search-jisho-redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yahoo Search Jisho Redirect
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Redirects yahoo searches to jisho searches for easy lookups from MCBookViewer
 // @author       You
 // @match        https://dic.yahoo.co.jp/search/?ei=UTF-8&p=*
@@ -12,5 +12,54 @@
 (function() {
     'use strict';
     const terms = (location.search.match(/p=(.*)/)[1]).replace('&stype=prefix&fr=dic', '');
-    location.replace('https://jisho.org/search/' + terms.trim());
+    location.replace('https://jisho.org/search/' + encodeURIComponent(trim(decodeURIComponent(terms))));
 })();
+
+function trim(phrase) {
+    const remove = regex => {
+        phrase = phrase.replace(regex, '');
+    };
+    phrase = phrase.trim();
+
+    //dangling punctuation/symbols from the previous sentence
+    remove(/^[。、！]/);
+    //dangling commas
+    remove(/[、]$/);
+
+    //if they look up something in english don't count that as a word to review (they can look up the result if they really want it to show up)
+    remove(/^[\w\d\s]*$/);
+
+
+    //remove messy copying (extra punctuation for other sentences), and remove quotes when they're not necessary for the quote.
+    //we don't want to remove quotes if it's something like "「うるさいだまれ」って言った" because it changes the meaning.
+    [   ['「', '」'],
+        ['『', '』'],
+        ['【', '】'],
+    ].forEach(([open, close]) => {
+        const leadingOpen = new RegExp('^' + open),
+            leadingClose = new RegExp('^' + close),
+            trailingClose = new RegExp(close + '$'),
+            trailingOpen = new RegExp(open + '$');
+
+        //starts with an opening quote but doesn't have any closing quotes, assume only part of a quote was selected, no need to keep in quotes
+        if (leadingOpen.test(phrase) && !phrase.includes(close)) {
+            remove(leadingOpen);
+        }
+        //same with closing quote
+        if (trailingClose.test(phrase) && !phrase.includes(open)) {
+            remove(trailingClose);
+        }
+
+        //a full quote with quote marks was included and nothing outside it, just remove the quote, it's unnecessary
+        if (leadingOpen.test(phrase) && trailingClose.test(phrase)) {
+            remove(leadingOpen);
+            remove(trailingClose);
+        }
+
+        //remove any quotes for quotes that weren't included in the phrase
+        remove(trailingOpen);
+        remove(leadingClose);
+    });
+
+    return phrase;
+}
